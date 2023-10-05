b78729d2a26a57b372acc6ce8e517bfe21c36e6b
sreeraj <sreeraj2122@gmail.com>
2023-10-05 16:23:29 +0530

    second  commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,95 +9,127 @@ bar.addEventListener('click', () => {
 
 let projects = [
     {
-        name: 'Buttons',
-        image: 'assets/images/35456.jpg',
-        sourceCode: 'https://github.com/sreeraj-1122?tab=repositories',
-        liveDemo: 'https://github.com/sreeraj-1122?tab=repositories',
+        name: 'SignUp page',
+        image: 'assets/images/signup.png',
+        sourceCode: 'https://github.com/sreeraj-1122/login-signup-page.git',
+        liveDemo: 'https://sreeraj-1122.github.io/login-signup-page/',
 
     },
     {
-        name: 'Buttons',
-        image: 'assets/images/35456.jpg',
-        sourceCode: 'https://github.com/sreeraj-1122?tab=repositories',
-        liveDemo: 'https://github.com/sreeraj-1122?tab=repositories',
+        name: 'Profile card',
+        image: 'assets/images/profilecard.png',
+        sourceCode: 'https://github.com/sreeraj-1122/profile-card.git',
+        liveDemo: 'https://sreeraj-1122.github.io/profile-card/',
 
     },
     {
-        name: 'Buttons',
-        image: 'assets/images/35456.jpg',
-        sourceCode: 'https://github.com/sreeraj-1122?tab=repositories',
-        liveDemo: 'https://github.com/sreeraj-1122?tab=repositories',
+        name: 'Navbar',
+        image: 'assets/images/navbar.png',
+        sourceCode: 'https://github.com/sreeraj-1122/responsive-navbar.git',
+        liveDemo: 'https://sreeraj-1122.github.io/responsive-navbar/',
 
     },
     {
-        name: 'Buttons',
-        image: 'assets/images/35456.jpg',
-        sourceCode: 'https://github.com/sreeraj-1122?tab=repositories',
-        liveDemo: 'https://github.com/sreeraj-1122?tab=repositories',
+        name: 'Simple calculator',
+        image: 'assets/images/simpcal.png',
+        sourceCode: 'https://github.com/sreeraj-1122/simple-calculator.git',
+        liveDemo: 'https://sreeraj-1122.github.io/simple-calculator/',
 
     },
     {
-        name: 'Buttons',
-        image: 'assets/images/35456.jpg',
-        sourceCode: 'https://github.com/sreeraj-1122?tab=repositories',
-        liveDemo: 'https://github.com/sreeraj-1122?tab=repositories',
+        name: 'Login page',
+        image: 'assets/images/login.png',
+        sourceCode: 'https://github.com/sreeraj-1122/login-signup-page.git',
+        liveDemo: 'https://sreeraj-1122.github.io/login-signup-page/',
 
     },
     {
-        name: 'Buttons',
-        image: 'assets/images/35456.jpg',
-        sourceCode: 'https://github.com/sreeraj-1122?tab=repositories',
-        liveDemo: 'https://github.com/sreeraj-1122?tab=repositories',
+        name: 'Color generator',
+        image: 'assets/images/color.png',
+        sourceCode: 'https://github.com/sreeraj-1122/color-generator.git',
+        liveDemo: 'https://sreeraj-1122.github.io/color-generator/',
+
+    },
+
+    {
+        name: 'BMI calculator',
+        image: 'assets/images/bmi.png',
+        sourceCode: 'https://github.com/sreeraj-1122/BMI-calculator.git',
+        liveDemo: 'https://sreeraj-1122.github.io/BMI-calculator/',
+
+    },
+
+    {
+        name: 'Hotel website',
+        image: 'assets/images/color.png',
+        sourceCode: 'https://github.com/sreeraj-1122/hotel-website.git',
+        liveDemo: 'https://sreeraj-1122.github.io/hotel-website/',
 
     },
     {
-        name: 'Buttons',
-        image: 'assets/images/35456.jpg',
-        sourceCode: 'https://github.com/sreeraj-1122?tab=repositories',
-        liveDemo: 'https://github.com/sreeraj-1122?tab=repositories',
+        name: 'Quiz app',
+        image: 'assets/images/quiz.png',
+        sourceCode: 'https://github.com/sreeraj-1122/Quiz-App.git',
+        liveDemo: 'https://sreeraj-1122.github.io/Quiz-App/',
 
     },
     {
-        name: 'Buttons',
-        image: 'assets/images/35456.jpg',
-        sourceCode: 'https://github.com/sreeraj-1122?tab=repositories',
-        liveDemo: 'https://github.com/sreeraj-1122?tab=repositories',
+        name: 'Calculator',
+        image: 'assets/images/calc.png',
+        sourceCode: 'https://github.com/sreeraj-1122/calculator-project.git',
+        liveDemo: 'https://sreeraj-1122.github.io/calculator-project/',
 
     },
+    {
+        name: 'Signup page',
+        image: 'assets/images/odin.png',
+        sourceCode: 'https://github.com/sreeraj-1122/Sign-up-page.git',
+        liveDemo: 'https://sreeraj-1122.github.io/Sign-up-page/',
+
+    },
+
+    {
+        name: 'Portfolio ',
+        image: 'assets/images/port.png',
+        sourceCode: 'https://github.com/sreeraj-1122/portfolio-website.git',
+        liveDemo: 'https://sreeraj-1122.github.io/portfolio-website/',
+
+    },
+
 ];
 let projectList = document.querySelector(".project-list");
 projects.forEach((project) => {
-    let card=document.createElement('div');
+    let card = document.createElement('div');
     card.classList.add('project-card');
 
-    let image=document.createElement('img');
-    image.src=project.image;
-    image.alt=project.name;
+    let image = document.createElement('img');
+    image.src = project.image;
+    image.alt = project.name;
     image.classList.add('project-img');
 
-    let projectDetails=document.createElement('div');
+    let projectDetails = document.createElement('div');
     projectDetails.classList.add('project-details');
 
-    let name=document.createElement('h3');
-    name.textContent=project.name;
+    let name = document.createElement('h3');
+    name.textContent = project.name;
     name.classList.add('project-name');
 
     // let tagGroup=document.createElement('div');
     // tagGroup.classList.add('tag-group');
 
-    let btnGroup=document.createElement('div');
+    let btnGroup = document.createElement('div');
     btnGroup.classList.add('btn-group');
 
-    let sourceCodeBtn=document.createElement('a');
-    sourceCodeBtn.textContent='Sourse code';
-    sourceCodeBtn.setAttribute('href',project.sourceCode);
-    sourceCodeBtn.setAttribute('target','__blank');
+    let sourceCodeBtn = document.createElement('a');
+    sourceCodeBtn.textContent = 'Sourse code';
+    sourceCodeBtn.setAttribute('href', project.sourceCode);
+    sourceCodeBtn.setAttribute('target', '__blank');
     sourceCodeBtn.classList.add('source-code-btn');
 
-    let liveDemoBtn=document.createElement('a');
-    liveDemoBtn.textContent='Live Demo';
-    liveDemoBtn.setAttribute('href',project.liveDemo);
-    liveDemoBtn.setAttribute('target','__blank');
+    let liveDemoBtn = document.createElement('a');
+    liveDemoBtn.textContent = 'Live Demo';
+    liveDemoBtn.setAttribute('href', project.liveDemo);
+    liveDemoBtn.setAttribute('target', '__blank');
     liveDemoBtn.classList.add('livedemo-btn');
 
     btnGroup.appendChild(sourceCodeBtn);
